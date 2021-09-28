4e0d29c5b2102fb6b3051505c69be4d48468c693
Yujia Qiao <rapiz3142@gmail.com>
2021-09-28 11:42:18 +0800

    workflow: Use GitHub Action to deploy
    Signed-off-by: Yujia Qiao <rapiz3142@gmail.com>

--- src/api.js
+++ src/api.js
@@ -1,38 +1,39 @@
-import {APP_KEY, APP_ID} from './config.js'
+import { APP_KEY, APP_ID } from "./config.js";
 
-const AV = require('leancloud-storage');
+const AV = require("leancloud-storage");
 
 export const SERVER_STATUS = {
   ONLINE: 0,
   OFFLINE: 1,
   ABORTED: 2,
-}
+};
 
+// eslint-disable-next-line no-unused-vars
 const fakeData = [
-    {
-      "host": "unraidarch",
-      "lastUpdate": 1617954550,
-      "status": "online",
-    },
-    {
-      "host": "tower",
-      "lastUpdate": 1617935392,
-      "status": "offline",
-    },
-    {
-      "host": "810openwrt",
-      "lastUpdate": 1617935392,
-      "status": "offline",
-    }
-]
+  {
+    host: "unraidarch",
+    lastUpdate: 1617954550,
+    status: "online",
+  },
+  {
+    host: "tower",
+    lastUpdate: 1617935392,
+    status: "offline",
+  },
+  {
+    host: "810openwrt",
+    lastUpdate: 1617935392,
+    status: "offline",
+  },
+];
 
 export function GetServers() {
-  const query = new AV.Query('Server')
-  return query.find().then(e => {
-    const ret = []
-    e.forEach(x => ret.push(x.toFullJSON()))
-    return ret
-  })
+  const query = new AV.Query("Server");
+  return query.find().then((e) => {
+    const ret = [];
+    e.forEach((x) => ret.push(x.toFullJSON()));
+    return ret;
+  });
 }
 
 export function ApiInit() {
